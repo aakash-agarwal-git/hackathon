54b55d281b9015f188df0c8593ad62c2358033b4
Chauhan <achauha21@TAGLDP28995.local>
2024-06-28 04:04:04 +0530

    fuzzy matches issue fix

--- app/controllers/categoryController.js
+++ app/controllers/categoryController.js
@@ -1,29 +1,32 @@
-const { getCategories } = require('../services/categoryService');
-
+const { getCategories } = require("../services/categoryService");
 
 const getCategory = async (req, res) => {
   try {
-    const key = req.params.key;
+    const key = req.params.key === ":" ? "" : req.params.key;
+
     const categories = await getCategories();
     if (!categories) {
-      return res.status(404).json({ message: 'Categories not found' });
+      return res.status(404).json({ message: "Categories not found" });
     }
     const fuzzyMatches = getTopFuzzyMatches(key, categories);
-    console.log('fuzzyMatches', fuzzyMatches);
+    console.log("fuzzyMatches", fuzzyMatches);
     res.json(fuzzyMatches);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server Error' });
+    res.status(500).json({ message: "Server Error" });
   }
 };
 
- function getTopFuzzyMatches(input, categories) {
-
-  const categoryNames = categories.map(category => category.name.toLowerCase());
+function getTopFuzzyMatches(input, categories) {
+  const categoryNames = categories.map((category) =>
+    category.name.toLowerCase()
+  );
 
   // Perform the fuzzy search
-  const matches = categoryNames.filter(name => name.toLowerCase().startsWith(input.toLowerCase()));
-  return matches.slice(0, 5);
+  const matches = categoryNames.filter((name) =>
+    name.toLowerCase().startsWith(input.toLowerCase())
+  );
+  return input ? matches.slice(0, 5) : categoryNames;
 }
 module.exports = {
   getCategory,
